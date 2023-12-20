5793bbb11d937454950a43a9593cadaab61491f1
t.onwuchekwa cos101 <treasure32109@gmail.com>
2023-12-20 23:31:36 +0100

    Tunde 1.0

---
+++ An Approach to AI/Extracts/For Login/Scripts Extract.js
@@ -0,0 +1,9 @@
+function goToChat() {
+    // Navigate to the chat page
+    window.location.href = 'file:///C:/Users/treas/OneDrive/Documents/Personal/An%20Approach%20to%20AI/Tunde.html'; // Replace 'chat.html' with the actual filename
+}
+
+function goToSignUp() {
+    // Navigate to the sign-up page
+    window.location.href = 'file:///C:/Users/treas/OneDrive/Documents/Personal/An%20Approach%20to%20AI/signup.html'; // Replace 'signup.html' with the actual filename
+}
